03f403637d48ea6ca5a9a214ce8fa7506f17cb17
Wong Wai Tuck <wongwaituck@gmail.com>
2020-04-07 00:07:31 -0400

    challenge1 done

--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+"""
+Challenge 1:
+Convert hex to base64
+The string:
+49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+
+Should produce:
+SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+So go ahead and make that happen. You'll need to use this code for the rest of the exercises.
+
+https://cryptopals.com/sets/1/challenges/1
+"""
+
 
 # Uses library bytearray.fromhex to convert a hex string to its bytearray representation
 def hex_to_bytearray(x):
